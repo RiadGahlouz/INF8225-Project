49e3ed8b48d0ebdd2e2f2a40f7fb088edeba69f7
Riad <combatarms970711@gmail.com>
2020-04-08 02:51:36 -0400

    Fix color palette and font to match original

--- game.py
+++ game.py
@@ -1,6 +1,20 @@
 from enum import Enum
 import random
 
+COLOR_DICT = {
+    2: (238, 228, 218),  # Font color: (119, 110, 101)
+    4: (237, 224, 200),  # Same font color as 2
+    8: (242, 177, 121),
+    16: (245, 149, 99),
+    32: (246, 124, 95),
+    64: (246, 94, 59),
+    128: (237, 207, 114),
+    256: (237, 204, 97),
+    512: (237, 200, 80),
+    1024: (237, 197, 63),
+    2048: (237, 194, 46)
+}
+
 
 class MoveDirection(Enum):
     LEFT = 0
@@ -11,6 +25,12 @@ class MoveDirection(Enum):
 
 def get_random_grid_element_coords() -> (int, int):
     return random.randint(0, 3), random.randint(0, 3)
+
+
+def get_color_for_number(number: int) -> (int, int, int):
+    if number in COLOR_DICT:
+        return COLOR_DICT[number]
+    return 204, 192, 179
 
 
 class GameGrid(object):
--- main.py
+++ main.py
@@ -10,35 +10,38 @@ def render_game_grid(window, font, grid: game.GameGrid):
     elements = grid.get_elements()
 
     for y, row in enumerate(elements):
-        print(row)
         for x, col in enumerate(row):
             rect_x = x * (BLOCK_SPACING + BLOCK_WIDTH) + BLOCK_SPACING
             rect_y = y * (BLOCK_SPACING + BLOCK_WIDTH) + BLOCK_SPACING
-            pygame.draw.rect(window, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+            pygame.draw.rect(window, game.get_color_for_number(col),
                              pygame.Rect(rect_x,
                                          rect_y,
                                          BLOCK_WIDTH, BLOCK_WIDTH))
-            text = font.render(str(col), True, (255, 255, 255))
+            # Don't render text for 0
+            if col == 0:
+                continue
+
+            text = font.render(str(col), True, (119, 110, 101) if col <= 4 else (255, 255, 255))
             window.blit(text, (rect_x + (float(BLOCK_WIDTH) / 2) - float(text.get_width()) / 2,
                                rect_y + (float(BLOCK_WIDTH) / 2) - float(text.get_height()) / 2))
 
 
 if __name__ == "__main__":
     pygame.init()
-    font = pygame.font.Font(None, 24)
+    font = pygame.font.Font("fonts/ClearSans-Bold.ttf", 32)
 
     window = pygame.display.set_mode(
         ((BLOCK_SPACING + BLOCK_WIDTH) * 4 + BLOCK_SPACING, (BLOCK_SPACING + BLOCK_WIDTH) * 4 + BLOCK_SPACING))
     pygame.display.set_caption("2048 NEAT Game")
     game_grid = game.GameGrid()
 
-    render_game_grid(window, font, game_grid)
     running = True
+    window.fill((187, 173, 160))  # TODO: Move this in the loop if we do move animations for the numbers
     while running:
         for evt in pygame.event.get():
             if evt.type == pygame.QUIT:
                 running = False
             if evt.type == pygame.KEYDOWN and evt.key == pygame.K_q:
                 running = False
-
+        render_game_grid(window, font, game_grid)
         pygame.display.flip()
